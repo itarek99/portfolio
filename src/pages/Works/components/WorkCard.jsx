7676a0982f093bd6e5791c82bec4dd1f276a5810
itarek99 <tarekulislam7060@gmail.com>
2024-12-22 00:37:33 +0600

    Working On Mobile Responsive

--- src/pages/Works/components/WorkCard.jsx
+++ src/pages/Works/components/WorkCard.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { FaGithub, FaGlobe } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa6';
 
 const WorkCard = ({ data }) => {
   return (
